809e6b339bc31489fe805a6c983fd3a99bbe77d8
Sajal Mishra <sajalonline@gmail.com>
2017-08-02 12:27:33 +0530

    Created by Coding Ground Client

---
+++ New Project-20170802/WorkForce.cs
@@ -0,0 +1,20 @@
+using System.IO;
+using System;
+
+namespace WorkForce
+{
+    class WorkForce : IWorkForce
+    {
+        private readonly IWorkForce _iWorkForce;
+        
+        public void Execute()
+        {
+            
+        }
+        
+        public void Run()
+        {
+            
+        }
+    }
+}
